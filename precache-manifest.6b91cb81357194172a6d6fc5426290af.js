43c33cc8bd0af8928da3967a25fe91cc72c2f0cd
Kai Yang/WHQ/Wistron <kai_yang@wistron.com>
2022-11-26 15:07:59 +0800

    Updates

---
+++ precache-manifest.6b91cb81357194172a6d6fc5426290af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7791368b328e441afa6",
+    "url": "/conut/static/css/main.eb55366e.chunk.css"
+  },
+  {
+    "revision": "b7791368b328e441afa6",
+    "url": "/conut/static/js/main.b7791368.chunk.js"
+  },
+  {
+    "revision": "47feb2933457be6d9b87",
+    "url": "/conut/static/js/1.47feb293.chunk.js"
+  },
+  {
+    "revision": "404a548143c4457e6fef",
+    "url": "/conut/static/js/2.404a5481.chunk.js"
+  },
+  {
+    "revision": "062685497ca969f2c2c8",
+    "url": "/conut/static/js/runtime~main.06268549.js"
+  },
+  {
+    "revision": "78bc04c0d35097d726c659db898fca9a",
+    "url": "/conut/index.html"
+  }
+];
