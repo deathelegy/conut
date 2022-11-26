52e2e2d69069c9bb12bcbd52ae1f5fba55bbea83
Kai Yang/WHQ/Wistron <kai_yang@wistron.com>
2022-11-26 14:54:38 +0800

    test

---
+++ build/precache-manifest.7a9f15360515c3f6fa9060d2570e87e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7791368b328e441afa6",
+    "url": "/static/css/main.eb55366e.chunk.css"
+  },
+  {
+    "revision": "b7791368b328e441afa6",
+    "url": "/static/js/main.b7791368.chunk.js"
+  },
+  {
+    "revision": "47feb2933457be6d9b87",
+    "url": "/static/js/1.47feb293.chunk.js"
+  },
+  {
+    "revision": "f13715e7e3bd15a29b6d",
+    "url": "/static/js/2.f13715e7.chunk.js"
+  },
+  {
+    "revision": "05b6e7d32a358bd3b582",
+    "url": "/static/js/runtime~main.05b6e7d3.js"
+  },
+  {
+    "revision": "f859a84ea703a132072ef86d6d576e3e",
+    "url": "/index.html"
+  }
+];
